e9f1d13de2d93601c22a3c204b701092164aec99
saannrr <ihsan.ican2002@gmail.com>
2024-04-29 12:11:12 +0700

    nambahin fitur aktivasi (blom kelar)

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -79,12 +79,41 @@ class Auth extends CI_Controller
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 1,
+                'is_active' => 0,
                 'date_created' => time()
             ];
-            $this->db->insert('user', $data);
+            // $this->db->insert('user', $data);
+            $this->_sendEmail();
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulations your account has been created!</div>');
             redirect('auth');
+        }
+    }
+
+    private function _sendEmail()
+    {
+        $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'ihsan.danger2002@gmail.com',
+            'smtp_pass' => 'bruhbruh6432@',
+            'smtp_port' => 465,
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'newline' => "\r\n"
+        ];
+
+        $this->load->library('email', $config);
+
+        $this->email->from('ihsan.danger2002@gmail.com', 'San Yeboi');
+        $this->email->to('ihsan.ican2002@gmail.com');
+        $this->email->subject('Testing');
+        $this->email->message('Hello World');
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
         }
     }
 
